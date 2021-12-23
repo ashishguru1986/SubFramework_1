ea1042c221dd132335de2fdf16c43b287d1222a9
Ashish Sharma <ashish.guru1986@gmail.com>
2021-12-23 08:34:56 +0530

    First commit

--- Sources/SubFramework_1/SubFramework_1.swift
+++ Sources/SubFramework_1/SubFramework_1.swift
@@ -1,6 +1,10 @@
 public struct SubFramework_1 {
     public private(set) var text = "Hello, World!"
-
+    
     public init() {
+    }
+    
+    public func subFramework_1_Method(){
+        print("subFramework_1_Method called")
     }
 }
